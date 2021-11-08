a7fc9f16448ea784d554bc5095ca0868a01ceb0c
WIN-6OHIEJTVVGG\User <vdiukarev90@gmail.com>
2021-11-08 15:47:51 +0200

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header">
       <div className="logo">
-        <img width={70} height={50} src="/assets/murky.png" alt="logo" />
+        <img width={70} height={50} src={process.env.PUBLIC_URL + '/assets/murky.png'} alt="logo" />
         <p>
           The best
           <br /> weather forecast
@@ -14,7 +14,7 @@ function Header() {
       <div className="my_cities">
         <Link to="/cities">
           <span>My cities</span>
-          <img width={30} height={30} src="/assets/sun.png" alt="sun" />
+          <img width={30} height={30} src={process.env.PUBLIC_URL + '/assets/sun.png'} alt="sun" />
         </Link>
       </div>
     </div>
